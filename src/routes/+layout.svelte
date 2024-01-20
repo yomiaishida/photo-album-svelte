9e85aedbfe4062d28d730d4b377b254a85646bc6
Abayomi Aishida <yomicorperate@gmail.com>
2024-01-20 23:21:11 +0100

    Basic navbar and content layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,25 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+  import Button from "$lib/components/ui/button/button.svelte";
+  import "../app.pcss";
+  import { Camera, Upload } from "lucide-svelte";
+</script>
+
+<!-- nav bar -->
+<div class="border-b fixed top-0 w-full bg-white">
+  <div class="container h-16 flex items-center">
+    <a href="/">
+      <Camera />
+    </a>
+
+    <div class="ml-auto">
+      <Button class="space-x-1">
+        <Upload class="h-[1rem]" />
+        <span>Upload</span>
+      </Button>
+    </div>
+  </div>
+</div>
+
+<div class="pt-16 container">
+  <slot />
+</div>
